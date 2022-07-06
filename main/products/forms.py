73eae22c96d89aeb96ae7c54d66eca517524070d
Electronic12 <sowgatfilm@gmail.com>
2022-07-06 11:33:54 +0500

    add update

--- main/products/forms.py
+++ main/products/forms.py
@@ -10,3 +10,13 @@ class ProductForm(FlaskForm):
     product_sale = StringField('Arzanladyş(%)', validators=[DataRequired()])
     product_description = TextAreaField('Tanyşdyryş', validators=[DataRequired()])
     submit = SubmitField('Harydy goş')
+
+
+class ProductUpdateForm(FlaskForm):
+    product_name = StringField('Ady', validators=[DataRequired()])
+    product_price = StringField('Bahasy', validators=[DataRequired()])
+    product_key = StringField('Harydyň kody', validators=[DataRequired()])
+    product_type = StringField('Harydyň görnüşi', validators=[DataRequired()])
+    product_sale = StringField('Arzanladyş(%)', validators=[DataRequired()])
+    product_description = TextAreaField('Tanyşdyryş', validators=[DataRequired()])
+    submit = SubmitField('Tazelemek')
--- main/products/routes.py
+++ main/products/routes.py
@@ -1,6 +1,6 @@
 from turtle import pos
-from flask import render_template, Blueprint
-from .forms import ProductForm
+from flask import render_template, Blueprint, request, redirect, url_for
+from .forms import ProductForm, ProductUpdateForm
 from main.models import Products
 from main import db
 
@@ -37,35 +37,28 @@ def delete_product(id):
     return render_template("products.html", our_products=our_products)
 
 
-@products.route('/edit_product/', methods=['GET', 'POST'])
-def edit_product():
-    form = ProductForm()
+@products.route('/product/<int:id>/update/', methods=['GET', 'POST'])
+def update_product(id):
+    products = Products.query.get_or_404(id)
+    form = ProductUpdateForm()
     if form.validate_on_submit():
-        edit_product = Products.query.filter_by(product_key=form.product_key.data).first()
-        if new_product is None:
-            new_product = Products(product_name=form.product_name.data, product_price=form.product_price.data, product_key=form.product_key.data, product_type=form.product_type.data, product_sale=form.product_sale.data, description=form.product_description.data)
-            db.session.add(new_product)
-            db.session.commit()
-        name = form.product_name.data
-        form.product_name.data = ''
-        form.product_price.data = ''
-        form.product_key.data = ''
-        form.product_type.data = ''
-        form.product_sale.data = ''
-        form.product_description.data = ''
-    title = "hi"
-    return render_template('add_product.html', form=form)
-
-@products.route('/product/<int:id>/delete/', methods=['GET', 'POST'])
-def delete_product(id):
-    print(id)
-    delete_product = Products.query.get_or_404(id)
-    print(delete_product)
-    db.session.delete(delete_product)
-    db.session.commit()
-    our_products = Products.query.all()
-    return render_template("products.html", our_products=our_products)
-
+        products.product_name = form.product_name.data
+        products.product_price = form.product_price.data
+        products.product_key = form.product_key.data
+        products.product_type = form.product_type.data
+        products.product_sale = form.product_sale.data
+        products.description = form.product_description.data
+        db.session.commit()
+        clear_products = Products.query.filter_by(product_type=2)
+        return render_template("clear_pro.html", clear_products=clear_products)
+    elif request.method == 'GET':
+        form.product_name.data = products.product_name
+        form.product_price.data = products.product_price
+        form.product_key.data = products.product_key
+        form.product_type.data = products.product_type
+        form.product_sale.data = products.product_sale
+        form.product_description.data = products.description
+        return render_template("edit_product.html", form=form)
 
 @products.route('/add_product/', methods=['GET', 'POST'])
 def add_products():
